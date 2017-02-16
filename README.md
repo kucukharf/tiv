6539795afad20dab8ea9f5f469f97b4055f34e74
Burak Arslan <burak.arslan@eu.sony.com>
2017-02-17 00:43:41 +0200

    initialized

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # tiv
-Validation of Turkish Identification Number
+Validation of a unique personal identification number that is assigned to every citizen of Turkey. The identification number is a unique 11-digit number given by the MERNIS computer on the basis of the citizen's registration record that is kept by the registration office. The number does not reflect any personal information about the citizen. It is not possible to change the identification number once applied.
+
+## Synopsis
+
+...
+
+## Code Example
+
+Usage Example 
+
+`var TCNO  = new TIValidation(); TCNO.validate('argument'));`
+
+## Installation
+
+You can use with native Javascript you don't need to any other libraries.
+
+
+## Tests
+...
+
+## Contributors
+
+@kucukharf
+
+## License
+
+MIT License
+
+Copyright (c) 2017 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ tiv.js
@@ -0,0 +1,130 @@
+/**
+ * 
+ * @author  Burak Arslan
+ * @requires 
+ * @name TIValidation
+ * @description Turkish Identification Number Validation
+ * @param  {string} 
+ * Usage -- 
+ * instance ex      : var TCNO  = new TIValidation();
+ * validate ex      :             TCNO.validate('argument'));
+ * 
+ */
+
+
+TIValidation.prototype = {
+
+    /**
+     * @name                         validate
+     * @description                  Validate Incoming Arguement
+     * @Usage                        {$MethodName}.validate(param);
+     * @param  {string, integer}     argument   
+     * @return {boolean}             true or false
+     */
+
+    validate: function(argument) {
+
+        return (this.checkIdentificationNumber(argument) ? this.CalculateLastDigits(argument) : false);
+    },
+
+
+    /**
+     * @name                         checkIdentificationNumber
+     * @description                  Validate Incoming Arguement
+     * @Usage                        {$MethodName}.checkIdentificationNumber(param);
+     * @param  {string, integer}     argument   
+     * @return {boolean}             ex: true or false
+     */
+
+    checkIdentificationNumber: function(argument) {
+
+        var _length = argument.trim().toString().length;
+        return (_length > 8 &&  _length < 12 ? this.checkIsNumeric(argument) : false);
+    },
+
+    /**
+     * @name                         checkIsNumeric
+     * @description                  checks param is numeric
+     * @Usage                        {$MethodName}.checkIsNumeric(param);
+     * @param  {string, integer}     argument   
+     * @return {boolean}             ex: true or false
+     */
+
+    checkIsNumeric: function(argument) {
+        return (/^\d+$/.test(argument) ? true : false);
+    },
+
+    /**
+     * @name                         CalculateLastDigits
+     * @description                  Calculating 10th and 11th digits of number with algorythm
+     * @Usage                        {$MethodName}.CalculateLastDigits(param);
+     * @param  {string, integer}     argument   
+     * @return {boolean}             ex: true or false
+     */
+
+    CalculateLastDigits: function(argument) {
+
+        var _argument = argument.substring(0, 9).split('').map(parseFloat);
+        var _firstGroup = this.sumArrayValues(this.getIndiceArray(this.options._first, _argument));
+        var _secondGroup = this.sumArrayValues(this.getIndiceArray(this.options._second, _argument));
+
+        _argument.push(((_firstGroup * 7) - _secondGroup) % 10);
+        _argument.push(this.sumArrayValues(_argument) % 10);
+        return (_argument.length === 11 ? this.ValidateResults(_argument, argument.split('').map(parseFloat)) : false);
+    },
+
+    /**
+     * @name                         ValidateResults
+     * @description                  Validate the results are equal and is it a valid number
+     * @Usage                        {$MethodName}.ValidateResults([a, b, c], [a, b, c]);
+     * @param  {array, array}        argumentCalculated and argumentBase
+     * @return {boolean}             ex: true or false
+     */
+    ValidateResults: function(_argument, argument) {
+        return (!![_argument.join(''), argument.join('')].reduce(function(a, b) {
+            return (a === b) ? a : NaN;
+        }));
+    },
+
+    /**
+     * @name                         sumArrayValues
+     * @description                  Validate Incoming Arguement
+     * @Usage                        {$MethodName}.sumArrayValues([1,3,4,5]);
+     * @param  {array}               argument   
+     * @return {boolean}             ex: true or false
+     */
+    sumArrayValues: function(argument) {
+        return argument.reduceRight(function(a, b) {
+            return a + b;
+        })
+    },
+
+
+    /**
+     * @name                         getIndiceArray
+     * @description                  Splice array on Multiple Indices multisplice with pattern
+     * @Usage                        {$MethodName}.getIndiceArray([1, 4, 8], [1, 2, 3, 4, 5, 6, 7, 8]);
+     * @param  {pattern, array}      pattern and argument   
+     * @return {boolean}             ex: true or false
+     */
+    getIndiceArray: function(pattern, arguments) {
+        var indices = [];
+        for (var i = 0; i < pattern.length; i++) {
+            var arg = pattern[i];
+            var index = arguments[arg - 1];
+            indices.push(index);
+        }
+        return indices;
+    }
+};
+
+
+
+function TIValidation() {
+    this.options = {
+        _first: [1, 3, 5, 7, 9],
+        _second: [2, 4, 6, 8]
+    };
+
+    this.debug = false;
+}
